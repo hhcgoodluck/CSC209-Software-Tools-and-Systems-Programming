b88fb3cb6147c0668f7ecaace5e24da7e2a647e6
hhc <hongcheng.huo@mail.utoronto.ca>
2026-04-03 04:31:49 -0400

    upload part of the content of CSC209 2025winter

---
+++ Sockets/server.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>    /* Internet domain header */
+#include <arpa/inet.h>     /* only needed on my mac */
+
+int main() {
+    // create socket
+    int listen_soc = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_soc == -1) {
+        perror("server: socket");
+        exit(1);
+    }
+
+
+    //initialize server address
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(54321);
+    memset(&server.sin_zero, 0, 8);
+    server.sin_addr.s_addr = INADDR_ANY;
+
+    // bind socket to an address
+    if (bind(listen_soc, (struct sockaddr *) &server, sizeof(struct sockaddr_in)) == -1) {
+        perror("server: bind");
+        close(listen_soc);
+        exit(1);
+    }
+
+
+    // Set up a queue in the kernel to hold pending connections.
+    if (listen(listen_soc, 5) < 0) {
+        // listen failed
+        perror("listen");
+        exit(1);
+    }
+
+    struct sockaddr_in client_addr;
+    unsigned int client_len = sizeof(struct sockaddr_in);
+    client_addr.sin_family = AF_INET;
+
+    int return_value = accept(listen_soc, (struct sockaddr *)&client_addr, &client_len);
+    return 0;
+}
